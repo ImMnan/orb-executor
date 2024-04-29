42fb277e6dc7a2c79000d261a62e1fa9aa84f3b2
Manan patel <86557286+ImMnan@users.noreply.github.com>
2024-04-29 13:58:48 +0530

    Initiating a new Repo for Orca

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2024 Manan immnan333@gmail.com
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -15,8 +16,38 @@ var rootCmd = &cobra.Command{
 	Short: "A Distributed Automation testing application",
 	Long:  ``,
 	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		version, _ := cmd.Flags().GetBool("version")
+		license, _ := cmd.Flags().GetBool("license")
+		if version {
+			fmt.Println("Version: Not-Defined")
+			fmt.Println("Author:  https://github.com/ImMnan/")
+			fmt.Println(`
+Orca  Copyright (C) 2024 Manan Patel
+This program comes with ABSOLUTELY NO WARRANTY; 
+This is free software, and you are welcome to redistribute it
+under certain conditions; type "bmgo --license" for details.
+	`)
+		} else if license {
+			fmt.Println(`Orca - A distributed Automation testing executor
+			Copyright (C) 2024  Manan Patel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`)
+		} else {
+			cmd.Help()
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -38,4 +69,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("version", "v", false, "Version of the installed bmgo!")
+	rootCmd.Flags().BoolP("license", "l", false, "Show license details")
 }
